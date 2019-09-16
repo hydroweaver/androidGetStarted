98c8e9bec490649e0520f5a0f6a7de1c75a35e2d
Verma <karan.verma@unilever.com>
2019-09-16 17:37:55 +0530

    android getting started app complete

--- app/src/main/java/com/example/androidgetstarted/MainActivity.kt
+++ app/src/main/java/com/example/androidgetstarted/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.androidgetstarted
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.androidgetstarted.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +16,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun sendMessage(view: View){
-        
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
